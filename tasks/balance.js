768e052537766cf158db46013a191f099dfdf250
Kevin Keaveney <kevinkeaveney@MacBook-Pro.local>
2021-05-17 14:48:32 +0100

    Collectables

--- tasks/balance.js
+++ tasks/balance.js
@@ -1,9 +1,12 @@
-task("balance", "Prints an accounts balance")
-    .addParam("account", "The accounts address")
+
+
+task("balance", "Prints an account's balance")
+    .addParam("account", "The account's address")
     .setAction(async taskArgs => {
         const account = web3.utils.toChecksumAddress(taskArgs.account)
         const balance = await web3.eth.getBalance(account)
-        console.log(web3.utils.fromWei(balance, "ether", "ETH"))
+
+        console.log(web3.utils.fromWei(balance, "ether"), "ETH")
     })
 
-    module.exports = {}
+module.exports = {}
--- tasks/fund-link.js
+++ tasks/fund-link.js
@@ -1,3 +1,4 @@
+
 let { networkConfig, getNetworkIdFromName } = require('../helper-hardhat-config')
 
 task("fund-link", "Funds a contract with LINK")
@@ -6,22 +7,23 @@ task("fund-link", "Funds a contract with LINK")
     .setAction(async (taskArgs) => {
         const contractAddr = taskArgs.contract
         let networkId = await getNetworkIdFromName(network.name)
-        console.log("Funding contract", contractAddr, " on network ", network.name)
-        let linkTokenAddress = networkConfig[networkId]['linkToken'] || taskArgs.linkToken
+        console.log("Funding contract ", contractAddr, " on network ", network.name)
+        let linkTokenAddress = networkConfig[networkId]['linkToken'] || taskArgs.linkaddress
         const LinkToken = await ethers.getContractFactory("LinkToken")
 
-        // Fund with 1 LINK token
-        const amount = web.utils.toHex(1e18)
+        //Fund with 1 LINK token
+        const amount = web3.utils.toHex(1e18)
 
-        // Get signer information
+        //Get signer information
         const accounts = await ethers.getSigners()
         const signer = accounts[0]
 
-        // Create connection to LINK token contract and iniate the transfer
+        //Create connection to LINK token contract and initiate the transfer
         const linkTokenContract = new ethers.Contract(linkTokenAddress, LinkToken.interface, signer)
         var result = await linkTokenContract.transfer(contractAddr, amount).then(function (transaction) {
-            console.log('Contract', contractAddr, ' funded with 1 LINK. Transaction Hash: ', transaction.hash)
+            console.log('Contract ', contractAddr, ' funded with 1 LINK. Transaction Hash: ', transaction.hash)
         })
     })
 
-    module.exports = {}
+module.exports = {}
+
